21c3360a89d0a463cb91b9ab2186678483107e1f
Kiet Ly <kiet.t.ly@gmail.com>
2021-01-10 15:59:29 -0500

    correct menu does not show

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
-import HomePage from './pages/homepage/homepage.component'
+
+import HomePage from './pages/homepage/homepage.component';
 
 const HatsPage = () => (
   <div>
-    <h1>HATS PAGE</h1>
+    <h1>HATS PAGE </h1>
   </div>
-)
+);
 
 function App() {
   return (
